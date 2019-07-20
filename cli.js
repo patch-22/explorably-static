f62ad74ec22c91d8c91a7a16e5f65e6c07f39ce1
Adam Kelly <adam.guitar12@gmail.com>
2019-07-20 15:48:57 +0100

    Built CLI Base

--- cli.js
+++ cli.js
@@ -1,1 +1,30 @@
-console.log('welcome')
+#!/usr/bin/env node
+
+const program = require('commander')
+const fs = require('fs-extra')
+
+const version = require('./package.json').version
+
+program.version(version)
+
+// Build the site
+program
+    .command('build')
+    .description('build the story in the current directory')
+    .action(() => {
+        //
+    })
+
+// Create a story
+program
+    .command('new')
+    .description('create a new story in the current directory')
+    .action(() => {
+        //
+    })
+
+program.parse(process.argv)
+
+if (process.argv.length < 3) {
+    program.help()
+}
